938bd4a6f39019640d2d2b16eb532af183ce6d57
FACU <facundoramirez089@gmail.com>
2021-06-11 12:25:19 -0300

    avances api

--- api/src/routes/pokemonsRouter.js
+++ api/src/routes/pokemonsRouter.js
@@ -9,22 +9,50 @@ router.get('/', async (req, res) => {
     var name = req.query.name;
     
     if(name === undefined) { // en el caso de NO haberle pasado ningun query (name)
+        // let pokemons = [];
+        // for(let i = 1; i < 41 ; i++) {
+        //     var resultado = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        //     data = resultado.data;
+        //     var type = data.types.map( type => type.type.name);
+           
+        //     pokemons.push({
+        //         name: data.name,
+        //         image: data.sprites.other.dream_world.front_default,
+        //         types: type
+        //     });
+        // }
+    
         let pokemons = [];
-        for(let i = 1; i < 41 ; i++) {
-            var resultado = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
-            data = resultado.data;
-            var type = data.types.map( type => type.type.name);
+
+        var data1 = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
+        var results1 = data1.data.results;
+
+        var data2 = await axios.get(data1.data.next);
+        var results2 = data2.data.results;
+
+        pokeUrl = [...results1, ...results2];
+        var pokeUrl = pokeUrl.map( pokemon => pokemon.url);
+        
+        // var pokeUrl = [];
+        // for(let i = 1 ; i < 41; i++) {
+        //     pokeUrl.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        // }
+        
+        var pokeResponses = await Promise.all(pokeUrl.map( pokemon => axios.get(pokemon)));
+        var data = await Promise.all(pokeResponses.map( response => {
+            var type = response.data.types.map( type => type.type.name);
            
             pokemons.push({
-                name: data.name,
-                image: data.sprites.other.dream_world.front_default,
+                name: response.data.name,
+                image: response.data.sprites.other.dream_world.front_default,
                 types: type
             });
-        }
+        }));
 
-        var prueba = await Pokemon.findAll({include: [Tipo]})
         
-        prueba = prueba.map( datos => {
+        var pokeDB = await Pokemon.findAll({include: [Tipo]})
+        
+        pokeDB = pokeDB.map( datos => {
             var pokemon = {
                 name: datos.nombre,
                 image: 'imagen.jpg',
@@ -119,16 +147,18 @@ router.post('/', async (req, res) => {
     })
     
     // Busco el tipo en la tabla "Tipo" y relaciono ambos registros colocandolos en la tabla intermedia
-    conn.sync({ alter: true }).then( async () => {  
-        for(let i = 0; i < req.body.types.length ; i++) {
-            var tipo = await Tipo.findOne({where: { nombre: req.body.types[i] }});
-            await pokemon.addTipo(tipo);
-        }
-        
-    });
     
+    var types = await Tipo.findAll({ where: { nombre: req.body.types } })
+    await pokemon.addTipo(types);
+
+    /* for(let i = 0; i < req.body.types.length ; i++) {
+        var tipo = await Tipo.findOne({where: { nombre: req.body.types[i] }});
+        await pokemon.addTipo(tipo);
+    } */
+       
     return res.send('<h1>Pokemon creado con éxito!</h1>');
-    
+        
+
     }
 );
 
